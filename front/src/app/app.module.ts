a860e3dba65a4c31e05b0607e05d317fe1a87134
Unknown <nico22utn@gmail.com>
2018-08-31 01:26:12 -0300

    Conflictos solucionados

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -15,11 +15,11 @@ import { MedioPagoComponent } from './components/configuracion/medio-pago/medio-
 import { EstadoDestacadoComponent } from './components/configuracion/estado-destacado/estado-destacado.component';
 import { RubroComponent } from './components/configuracion/rubro/rubro.component';
 import { UsuarioComponent } from './components/configuracion/usuario/usuario.component';
-import { EstadoRequerimientoComponent } from './components/estado-requerimiento/estado-requerimiento.component';
 import { LoginComponent } from './components/seguridad/login.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { RegistracionComponent } from './components/seguridad/registracion/registracion.component';
 import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, LinkedinLoginProvider,} from "angular-6-social-login";
+import { EstadoRequerimientoComponent } from './components/configuracion/estado-requerimiento/estado-requerimiento.component';
 
 // Configs 
 export function getAuthServiceConfigs() {
@@ -54,7 +54,7 @@ export function getAuthServiceConfigs() {
     MedioPagoComponent,
     EstadoDestacadoComponent,
     RubroComponent,
-    EstadoRequerimientoComponent
+    EstadoRequerimientoComponent,
     UsuarioComponent,
     LoginComponent,
     LayoutComponent,
