8800066fb24ab7820418b8ba1c5c671a346387ff
Nicolas <nico22utn@gmail.com>
2018-12-10 10:31:13 -0300

    fix terminado

--- front/src/app/services/calificacion.service.ts
+++ front/src/app/services/calificacion.service.ts
@@ -13,11 +13,15 @@ export class CalificacionService {
     this.urlCalificaciones=`${URL_API}/calificacion`
   }
 
-  getCalificaciones(idUsuario){
-    let url=`${this.urlCalificaciones}/calificacionesUsuario?idUsuario=${idUsuario}`;
+  // getCalificaciones(idUsuario){
+  //   let url=`${this.urlCalificaciones}/calificacionesUsuario?idUsuario=${idUsuario}`;
+  //   return this.http.get(url);
+  // }
+  getCalificaciones(idUsuario,demandante,oferente){
+    let url=`${URL_API}/usuario/reporte-calificaciones?id=${idUsuario}&esDemandante=${demandante}&esOferente=${oferente}`;
     return this.http.get(url);
   }
-  
+
   calificar(calificacion){
     let url=`${this.urlCalificaciones}/realizarCalificacion`;
     return this.http.put(url,calificacion);
